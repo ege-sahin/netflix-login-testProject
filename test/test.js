96919ae866b8c9bbe66e1e8c57fc67a0ff294d92
ege-sahin <sahin.ege98@gmail.com>
2022-02-27 20:25:37 +0300

    Need Help Button test

--- test/test.js
+++ test/test.js
@@ -1,13 +1,43 @@
 const { Builder, By, Key, util } = require("selenium-webdriver");
 require("chromedriver");
 
-async function example() {
-  let driver = await new Builder().forBrowser("chrome").build();
-  await driver.get(
-    "file:///C:/Users/User/Desktop/GithubFiles/GitHub/netflix-login-clone/index.html"
-  );
-  await driver.findElement(By.name("email")).sendKeys("johndoe@mail.com");
-  await driver.findElement(By.name("password")).sendKeys("123456789");
-  await driver.findElement(By.id("signin-btn")).click();
+var driver
+
+async function succesfullLogin() {
+    driver = await new Builder().forBrowser("chrome").build();
+
+    await driver.get(__dirname + "/../index.html");
+
+    await driver.findElement(By.name("email")).sendKeys("johndoe@mail.com");
+    await driver.findElement(By.name("password")).sendKeys("123456789");
+    await driver.findElement(By.id("signin-btn")).click();
+
+    setTimeout(closeScreen, 1000);
 }
-example();
+
+
+async function facebookLogin() {
+    driver = await new Builder().forBrowser("chrome").build();
+
+    await driver.get(__dirname + "/../index.html");
+    await driver.findElement(By.id("facebook-btn")).click();
+
+    //setTimeout(closeScreen, 1000);
+}
+
+async function needHelpButton() {
+    driver = await new Builder().forBrowser("chrome").build();
+
+    await driver.get(__dirname + "/../index.html");
+    await driver.findElement(By.id("needHelpLink")).click();
+    setTimeout(closeScreen, 1000);
+}
+
+async function closeScreen(){
+    await driver.quit();
+}
+
+//succesfullLogin();
+//needHelpButton();
+//facebookLogin();
+
