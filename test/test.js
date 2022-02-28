48866632a5f424378d5fab1424c7a22262559bbd
ege-sahin <sahin.ege98@gmail.com>
2022-02-28 21:40:53 +0300

    Different browser test case

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
-const { Builder, By, Key, util } = require("selenium-webdriver");
+const { Builder, By, Key, until } = require("selenium-webdriver");
 require("chromedriver");
 
 var driver
 
-async function succesfullLogin() {
+async function succesfulLogin() {
     driver = await new Builder().forBrowser("chrome").build();
 
     await driver.get(__dirname + "/../index.html");
@@ -15,14 +15,26 @@ async function succesfullLogin() {
     setTimeout(closeScreen, 1000);
 }
 
+async function succesfulLoginFirefox() {
+    driver = await new Builder().forBrowser("firefox").build();
+
+    await driver.get(__dirname + "/../index.html");
+
+    await driver.findElement(By.name("email")).sendKeys("johndoe@mail.com");
+    await driver.findElement(By.name("password")).sendKeys("123456789");
+    await driver.findElement(By.id("signin-btn")).click();
+
+    setTimeout(closeScreen, 1000);
+}
 
 async function facebookLogin() {
     driver = await new Builder().forBrowser("chrome").build();
 
     await driver.get(__dirname + "/../index.html");
-    await driver.findElement(By.id("facebook-btn")).click();
+    await driver.findElement(By.id("facebook-btn")).click();   
+    
 
-    //setTimeout(closeScreen, 1000);
+    //setTimeout(closeScreen, 10000);
 }
 
 async function needHelpButton() {
@@ -37,7 +49,8 @@ async function closeScreen(){
     await driver.quit();
 }
 
-//succesfullLogin();
-//needHelpButton();
-//facebookLogin();
 
+//succesfulLogin();
+//needHelpButton();
+facebookLogin();
+//succesfulLoginFirefox();
