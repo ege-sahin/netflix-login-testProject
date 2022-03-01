2fdf80ac3e0ead28fdc66509594bbe191892badf
ege-sahin <sahin.ege98@gmail.com>
2022-03-01 12:36:38 +0300

    Facebook login test, UnsuccesfullLogin tests, RememberMe Started

--- test/test.js
+++ test/test.js
@@ -12,21 +12,34 @@ async function succesfulLogin() {
     await driver.findElement(By.name("password")).sendKeys("123456789");
     await driver.findElement(By.id("signin-btn")).click();
 
-    setTimeout(closeScreen, 1000);
+    await driver.sleep(5000);
+    await driver.quit();
+}
+
+async function unsuccesfulLogin() {
+    driver = await new Builder().forBrowser("chrome").build();
+
+    await driver.get(__dirname + "/../index.html");
+
+    await driver.findElement(By.name("email")).sendKeys("johndoemail.com");
+    await driver.findElement(By.name("password")).sendKeys("123456789");
+    await driver.findElement(By.id("signin-btn")).click();
+
+    await driver.sleep(5000);
+    await driver.quit();
 }
 
 async function succesfulLoginFirefox() {
     driver = await new Builder().forBrowser("firefox").build();
 
     await driver.get(__dirname + "/../index.html");
-
     await driver.findElement(By.name("email")).sendKeys("johndoe@mail.com");
     await driver.findElement(By.name("password")).sendKeys("123456789");
     await driver.findElement(By.id("signin-btn")).click();
 
-    setTimeout(closeScreen, 1000);
+    await driver.sleep(5000);
+    await driver.quit();
 }
-
 async function facebookLogin() {
     driver = await new Builder().forBrowser("chrome").build();
 
@@ -36,15 +49,41 @@ async function facebookLogin() {
     var parent = await driver.getWindowHandle();
     var windows = await driver.getAllWindowHandles();
 
+    await driver.switchTo().window(windows[1]);
+
+    await driver.findElement(By.id("email")).sendKeys("helena438@gmail.com");
+    await driver.findElement(By.id("pass")).sendKeys("nekroz453");
+    await driver.findElement(By.id("loginbutton")).click();
+
+    await driver.sleep(8000);
+
+    driver.close();
+    driver.switchTo().window(parent);
     await driver.sleep(2000);
+    await driver.quit();
+}
+
+async function unsuccesfulfacebookLogin() {
+    driver = await new Builder().forBrowser("chrome").build();
+
+    await driver.get(__dirname + "/../index.html");
+    await driver.findElement(By.id("facebook-btn")).click();   
+    
+    var parent = await driver.getWindowHandle();
+    var windows = await driver.getAllWindowHandles();
 
     await driver.switchTo().window(windows[1]);
 
-    await driver.findElement(By.id("email")).sendKeys("abc");
+    await driver.findElement(By.id("email")).sendKeys("johndoe@mail.com");
+    await driver.findElement(By.id("pass")).sendKeys("123456789");
+    await driver.findElement(By.id("loginbutton")).click();
 
-    //driver.close();
-    //driver.switchTo().window(parent);
-    //setTimeout(closeScreen, 10000);
+    await driver.sleep(15000);
+
+    driver.close();
+    driver.switchTo().window(parent);
+    await driver.sleep(2000);
+    await driver.quit();
 }
 
 async function needHelpButton() {
@@ -52,15 +91,36 @@ async function needHelpButton() {
 
     await driver.get(__dirname + "/../index.html");
     await driver.findElement(By.id("needHelpLink")).click();
-    setTimeout(closeScreen, 1000);
-}
-
-async function closeScreen(){
+    await driver.sleep(5000);
     await driver.quit();
 }
 
+async function rememberMe() {
+    driver = await new Builder().forBrowser("chrome").build();
 
-//succesfulLogin();
-//needHelpButton();
+    await driver.get(__dirname + "/../index.html");
+
+    await driver.findElement(By.name("email")).sendKeys("johndoe@mail.com");
+    await driver.findElement(By.name("password")).sendKeys("123456789");
+
+    await driver.findElement(By.id("rememberMe")).click();
+
+    await driver.sleep(1000);
+
+    await driver.findElement(By.id("signin-btn")).click();
+
+    await driver.sleep(1000);
+
+    await driver.findElement(By.id("signout-btn")).click();
+    
+    await driver.sleep(2000);
+    await driver.quit();
+}
+
+succesfulLogin();
+needHelpButton();
 facebookLogin();
-//succesfulLoginFirefox();
+succesfulLoginFirefox();
+rememberMe();
+unsuccesfulLogin();
+unsuccesfulfacebookLogin();
